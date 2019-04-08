fdbfe53f546b47f33384f737c65eb608963335cd
goutam <goutamgmb@gmail.com>
2019-04-08 18:28:43 +0200

    eco network

--- install.sh
+++ install.sh
@@ -70,7 +70,16 @@ echo ""
 echo ""
 echo "****************** Downloading networks ******************"
 mkdir pytracking/networks
+
+echo ""
+echo ""
+echo "****************** ATOM Network ******************"
 bash pytracking/utils/gdrive_download 1ZTdQbZ1tyN27UIwUnUrjHChQb5ug2sxr pytracking/networks/atom_default.pth
+
+echo ""
+echo ""
+echo "****************** ECO Network ******************"
+bash pytracking/utils/gdrive_download 1aWC4waLv_te-BULoy0k-n_zS-ONms21S pytracking/networks/resnet18_vggmconv1.pth
 
 echo ""
 echo ""
