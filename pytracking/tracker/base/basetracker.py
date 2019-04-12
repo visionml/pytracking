b8d7e947419561257e6e156d0dc5a7791fce17a0
dongfangduoshou123 <471747996@qq.com>
2019-04-12 13:38:47 +0800

    add a function for track target in video file
    add a function for track target in video file for BaseTracker class. 
    as follows:
    
    def track_videofile(self,videofilepath):
            "Run track with a video file input."
    
            #cap = cv.VideoCapture("/home/dtt/Videos/haze/v_1509066461577.avi")
            cap = cv.VideoCapture(videofilepath)
            display_name = 'Display: ' + self.params.tracker_name
            cv.namedWindow(display_name, cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO)
            cv.resizeWindow(display_name, 960, 720)
    
            while True:
                success, frame = cap.read()
                if success is not True:
                    break
                cv.imshow(display_name, frame)
                k = cv.waitKey(100)
                if k == 27:
                    x, y, w, h = cv.selectROI(display_name, frame)
                    init_state = [x, y, w, h]
                    self.initialize(frame, init_state)
                    break
    
            if hasattr(self, 'initialize_features'):
                self.initialize_features()
    
            while True:
                ret, frame = cap.read()
                frame_disp = frame.copy()
    
                # Draw box
                state, flag= self.track(frame)
                if(flag is not 'not_found'):
                    state = [int(s) for s in state]
                    cv.rectangle(frame_disp, (state[0], state[1]), (state[2] + state[0], state[3] + state[1]),
                                 (0, 255, 0), 5)
                else:
                    print("target is disappeared.")
    
                # Display the resulting frame
                cv.imshow(display_name, frame_disp)
                key = cv.waitKey(1)
                if key == ord('q'):
                    break
                elif key == ord('r'):
                    ret, frame = cap.read()
                    cv.imshow(display_name, frame)
                    x, y, w, h = cv.selectROI(display_name, frame)
                    init_state = [x, y, w, h]
                    self.initialize(frame, init_state)
    
    
            # When everything done, release the capture
            cap.release()
            cv.destroyAllWindows()

--- pytracking/tracker/base/basetracker.py
+++ pytracking/tracker/base/basetracker.py
@@ -52,6 +52,60 @@ class BaseTracker:
 
         return tracked_bb, times
 
+    def track_videofile(self,videofilepath):
+        "Run track with a video file input."
+
+        #cap = cv.VideoCapture("/home/dtt/Videos/haze/v_1509066461577.avi")
+        cap = cv.VideoCapture(videofilepath)
+        display_name = 'Display: ' + self.params.tracker_name
+        cv.namedWindow(display_name, cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO)
+        cv.resizeWindow(display_name, 960, 720)
+
+        while True:
+            success, frame = cap.read()
+            if success is not True:
+                break
+            cv.imshow(display_name, frame)
+            k = cv.waitKey(100)
+            if k == 27:
+                x, y, w, h = cv.selectROI(display_name, frame)
+                init_state = [x, y, w, h]
+                self.initialize(frame, init_state)
+                break
+
+        if hasattr(self, 'initialize_features'):
+            self.initialize_features()
+
+        while True:
+            ret, frame = cap.read()
+            frame_disp = frame.copy()
+
+            # Draw box
+            state= self.track(frame)
+            if(flag is not 'not_found'):
+                state = [int(s) for s in state]
+                cv.rectangle(frame_disp, (state[0], state[1]), (state[2] + state[0], state[3] + state[1]),
+                             (0, 255, 0), 5)
+            else:
+                print("target is disappeared.")
+
+            # Display the resulting frame
+            cv.imshow(display_name, frame_disp)
+            key = cv.waitKey(1)
+            if key == ord('q'):
+                break
+            elif key == ord('r'):
+                ret, frame = cap.read()
+                cv.imshow(display_name, frame)
+                x, y, w, h = cv.selectROI(display_name, frame)
+                init_state = [x, y, w, h]
+                self.initialize(frame, init_state)
+
+
+        # When everything done, release the capture
+        cap.release()
+        cv.destroyAllWindows()
+
 
     def track_webcam(self):
         """Run tracker with webcam."""
@@ -98,6 +152,7 @@ class BaseTracker:
 
         if hasattr(self, 'initialize_features'):
             self.initialize_features()
+
 
         while True:
             # Capture frame-by-frame
