ad352788b30c70c8c48a4215d081cd4e58a9cccd
goutam <goutamgmb@gmail.com>
2019-09-03 20:24:37 +0200

    minor fix

--- pytracking/evaluation/environment.py
+++ pytracking/evaluation/environment.py
@@ -1,10 +1,13 @@
 import importlib
 import os
 
+
 class EnvSettings:
     def __init__(self):
-        self.results_path = 'tracking_results/'
-        self.network_path = '../networks/'
+        pytracking_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+        self.results_path = '{}/tracking_results/'.format(pytracking_path)
+        self.network_path = '{}/networks/'.format(pytracking_path)
         self.otb_path = ''
         self.nfs_path = ''
         self.uav_path = ''
@@ -15,6 +18,32 @@ class EnvSettings:
         self.trackingnet_path = ''
 
 
+def create_default_local_file():
+    comment = {'results_path': 'Where to store tracking results',
+               'network_path': 'Where tracking networks are stored.'}
+
+    path = os.path.join(os.path.dirname(__file__), 'local.py')
+    with open(path, 'w') as f:
+        settings = EnvSettings()
+
+        f.write('from pytracking.evaluation.environment import EnvSettings\n\n')
+        f.write('def local_env_settings():\n')
+        f.write('    settings = EnvSettings()\n\n')
+        f.write('    # Set your local paths here.\n\n')
+
+        for attr in dir(settings):
+            comment_str = None
+            if attr in comment:
+                comment_str = comment[attr]
+            attr_val = getattr(settings, attr)
+            if not attr.startswith('__') and not callable(attr_val):
+                if comment_str is None:
+                    f.write('    settings.{} = \'{}\'\n'.format(attr, attr_val))
+                else:
+                    f.write('    settings.{} = \'{}\'    # {}\n'.format(attr, attr_val, comment_str))
+        f.write('\n    return settings\n\n')
+
+
 def env_settings():
     env_module_name = 'pytracking.evaluation.local'
     try:
@@ -23,27 +52,7 @@ def env_settings():
     except:
         env_file = os.path.join(os.path.dirname(__file__), 'local.py')
 
-        comment = {'results_path': 'Where to store tracking results',
-                   'network_path': 'Where tracking networks are stored.'}
-
-        with open(env_file, 'w') as f:
-            settings = EnvSettings()
-
-            f.write('from pytracking.evaluation.environment import EnvSettings\n\n')
-            f.write('def local_env_settings():\n')
-            f.write('    settings = EnvSettings()\n\n')
-            f.write('    # Set your local paths here.\n\n')
-
-            for attr in dir(settings):
-                comment_str = None
-                if attr in comment:
-                    comment_str = comment[attr]
-                attr_val = getattr(settings, attr)
-                if not attr.startswith('__') and not callable(attr_val):
-                    if comment_str is None:
-                        f.write('    settings.{} = \'{}\'\n'.format(attr, attr_val))
-                    else:
-                        f.write('    settings.{} = \'{}\'    # {}\n'.format(attr, attr_val, comment_str))
-            f.write('\n    return settings\n\n')
-
-            raise RuntimeError('YOU HAVE NOT SETUP YOUR local.py!!!\n Go to "{}" and set all the paths you need. Then try to run again.'.format(env_file))
+        # Create a default file
+        create_default_local_file()
+        raise RuntimeError('YOU HAVE NOT SETUP YOUR local.py!!!\n Go to "{}" and set all the paths you need. '
+                           'Then try to run again.'.format(env_file))
