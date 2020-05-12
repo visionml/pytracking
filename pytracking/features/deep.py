dfcf7078492f7674b439e67b623c214ae533f966
lcsama <1035085443@qq.com>
2020-05-12 16:10:47 +0800

    add backbone the mobilenet

--- pytracking/features/deep.py
+++ pytracking/features/deep.py
@@ -6,6 +6,7 @@ from pytracking.evaluation.environment import env_settings
 import os
 from pytracking.utils.loading import load_network
 from ltr.models.backbone.resnet18_vggm import resnet18_vggmconv1
+from ltr.models.backbone.mobilenetv3 import mobilenet3
 
 normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                              std=[0.229, 0.224, 0.225])
@@ -82,6 +83,75 @@ class ResNet18m1(MultiFeatureBase):
         with torch.no_grad():
             return TensorList(self.net(im).values())
 
+class Mobilenet(MultiFeatureBase):
+    """ResNet18 feature together with the VGG-m conv1 layer.
+    args:
+        output_layers: List of layers to output.
+        net_path: Relative or absolute net path (default should be fine).
+        use_gpu: Use GPU or CPU.
+    """
+
+    def __init__(self, output_layers, net_path=None, use_gpu=True, *args, **kwargs):
+        super(ResNet18m1, self).__init__(*args, **kwargs)
+
+        for l in output_layers:
+            if l not in ['init_conv','layer1', 'layer2', 'layer3', 'layer4', 'layer5','layer6','layer_out']:
+                raise ValueError('Unknown layer')
+
+        self.output_layers = list(output_layers)
+        self.use_gpu = use_gpu
+        self.net_path = 'mobilev3_test.t7' if net_path is None else net_path
+
+    def initialize(self):
+
+        if isinstance(self.pool_stride, int) and self.pool_stride == 1:
+            self.pool_stride = [1] * len(self.output_layers)
+
+        self.layer_stride = {'init_conv': 2, 'layer1': 2, 'layer2': 4, 'layer3': 8, 'layer4': 16, 'layer5': 16,'layer6': 32, 'layer_out': 32}
+        self.layer_dim = {'init_conv': 16, 'layer1': 16, 'layer2': 24, 'layer3': 40, 'layer4': 80, 'layer5': 112,'layer6': 160, 'layer_out': 960}
+
+
+        self.mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, -1, 1, 1)
+        self.std = torch.Tensor([0.229, 0.224, 0.225]).view(1, -1, 1, 1)
+
+        if os.path.isabs(self.net_path):
+            net_path_full = [self.net_path]
+        else:
+            root_paths = env_settings().network_path
+            if isinstance(root_paths, str):
+                root_paths = [root_paths]
+            net_path_full = [os.path.join(root, self.net_path) for root in root_paths]
+
+        self.net = None
+        for net_path in net_path_full:
+            try:
+                self.net = mobilenet3(self.output_layers, path=net_path_full)
+                break
+            except:
+                pass
+        if self.net is None:
+            raise Exception('Did not find network file {}'.format(self.net_path))
+
+        if self.use_gpu:
+            self.net.cuda()
+        self.net.eval()
+
+    def dim(self):
+        return TensorList([self.layer_dim[l] for l in self.output_layers])
+
+    def stride(self):
+        return TensorList([s * self.layer_stride[l] for l, s in zip(self.output_layers, self.pool_stride)])
+
+    def extract(self, im: torch.Tensor):
+        im = im / 255
+        im -= self.mean
+        im /= self.std
+
+        if self.use_gpu:
+            im = im.cuda()
+
+        with torch.no_grad():
+            return TensorList(self.net(im).values())
 
 class ATOMResNet18(MultiFeatureBase):
     """ResNet18 feature with the ATOM IoUNet.
