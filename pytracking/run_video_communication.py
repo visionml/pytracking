104750266b83f5e66b6008ca2fe428e30bff19f5
Ahsan Raza <ahsanr4@outlook.com>
2022-07-20 12:39:27 +0500

    Add files via upload

---
+++ pytracking/run_video_communication.py
@@ -0,0 +1,41 @@
+'''
+Execute this file when tracking with a Client Server setup.
+'''
+
+import os
+import sys
+import argparse
+
+env_path = os.path.join(os.path.dirname(__file__), '..')
+if env_path not in sys.path:
+    sys.path.append(env_path)
+
+from pytracking.evaluation import Tracker
+
+
+def run_video_comm(tracker_name, tracker_param, optional_box=None, debug=None, save_results=False):
+    """Run the tracker with the communication with the Ground Station (socket programming) using rtsp.
+    args:
+        tracker_name: Name of tracking method.
+        tracker_param: Name of parameter file.
+        debug: Debug level.
+    """
+    tracker = Tracker(tracker_name, tracker_param)
+    tracker.run_video_comm(optional_box=optional_box, debug=debug, save_results=save_results)
+
+def main():
+    parser = argparse.ArgumentParser(description='Run the tracker on your webcam.')
+    parser.add_argument('tracker_name', type=str, help='Name of tracking method.')
+    parser.add_argument('tracker_param', type=str, help='Name of parameter file.')
+    parser.add_argument('--optional_box', type=float, default=None, nargs="+", help='optional_box with format x y w h.')
+    parser.add_argument('--debug', type=int, default=0, help='Debug level.')
+    parser.add_argument('--save_results', dest='save_results', action='store_true', help='Save bounding boxes')
+    parser.set_defaults(save_results=False)
+
+    args = parser.parse_args()
+
+    run_video_comm(args.tracker_name, args.tracker_param, args.optional_box, args.debug, args.save_results)
+
+
+if __name__ == '__main__':
+    main()
